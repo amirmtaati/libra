7f2413c500c60952c99f82509efd69102784f04f
amirmtaati <amirmtaati@gmail.com>
2025-06-18 13:04:23 +0330

    book service created

---
+++ core/service/book_service.go
@@ -0,0 +1,59 @@
+package service
+
+import (
+	"fmt"
+	"github.com/amirmtaati/libra/core/models"
+	"github.com/amirmtaati/libra/core/repository"
+	"strings"
+)
+
+type BookService struct {
+	bookRepo repository.BookRepository
+}
+
+func NewBookService(r repository.BookRepository) *BookService {
+	return &BookService{bookRepo: r}
+}
+
+func (s *BookService) Create(title, author, tags, format, filePath string) (*models.Book, error) {
+	book := &models.Book{
+		Title:    strings.TrimSpace(title),
+		Author:   strings.TrimSpace(author),
+		Tags:     strings.TrimSpace(tags),
+		Format:   strings.ToUpper(format),
+		FilePath: filePath,
+	}
+
+	if err := s.bookRepo.Create(book); err != nil {
+		return nil, fmt.Errorf("Failed to create book: %v", err)
+	}
+
+	return book, nil
+}
+
+func (s *BookService) GetByID(id uint) (*models.Book, error) {
+	book, err := s.bookRepo.GetByID(id)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to get book by ID: %v", err)
+	}
+
+	return book, nil
+}
+
+func (s *BookService) GetAll() ([]*models.Book, error) {
+	books, err := s.bookRepo.GetAll()
+	if err != nil {
+		return nil, fmt.Errorf("Failed to get all books: %v", err)
+	}
+
+	return books, nil
+}
+
+func (s *BookService) Delete(id uint) error {
+	err := s.bookRepo.Delete(id)
+	if err != nil {
+		return fmt.Errorf("Failed to delete book: %v", err)
+	}
+
+	return nil
+}
